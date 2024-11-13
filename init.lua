88cd1c9b68d60b7c4f49ac202eaaf4388bf41d09
Matthew Sunner <sunner.matt@gmail.com>
2024-11-13 10:46:17 -0600

    Update config to autocomplete parentheses and brackets

--- init.lua
+++ init.lua
@@ -190,6 +190,20 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Function to map keys - Auto-close brackets and quotes
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('i', '(', '()<Left>')
+map('i', '[', '[]<Left>')
+map('i', "'", "''<Left>")
+map('i', '"', '""<Left>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
